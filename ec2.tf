74b7c2735018196056e829e5621a93b714b8f940
vrk2299 <93345302+vrk2299@users.noreply.github.com>
2023-06-20 17:23:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,9 +14,9 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "test-instance"
   }
 }
